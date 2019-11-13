44f664708c148445da82fa5825bf2bb70343d76c
Jonathan Nicholas <jojonicho181@gmail.com>
2019-11-14 01:06:37 +0700

    backend

--- djticket/settings.py
+++ djticket/settings.py
@@ -159,7 +159,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    'PAGE_SIZE': 10,
+    # 'PAGE_SIZE': 10,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
