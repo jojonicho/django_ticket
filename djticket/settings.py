557246e25e0d7620557f8a357089636184dbfcc9
Jonathan Nicholas <jojonicho181@gmail.com>
2020-02-07 00:04:45 +0700

    django filter

--- djticket/settings.py
+++ djticket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7n(+l2v0j91xf9gcfqx$1(pd!isixz^f)p#vo1r8j87zsq$gcf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['jonathannicholas-pti.herokuapp.com',
+ALLOWED_HOSTS = ['joni-pti.herokuapp.com',
                  'localhost', '127.0.0.1']
 
 
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     # 'rest_framework.authtoken',
-    # 'django_filters',
     'corsheaders',
     'django_filters'
 ]
